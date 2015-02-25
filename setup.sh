ebcdfe880deed83eeba16cd042ff4bc37973218b
Bob <bob.bever+github@gmail.com>
2015-02-25 13:29:17 -0500

    Add descriptions and change paths
    Bugfix to add full path to symlinks as well as adding a state and config symlinks in /root as well as /
    Also added descriptions to what is going on in the file.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
+echo "Downloading dependencies"
 sudo apt-get install -y git libao-dev libmad0-dev libfaad-dev libgnutls-dev libjson0-dev libgcrypt11-dev pkg-config make
 
+echo "Cloning Pianobar"
 git clone https://github.com/PromyLOPh/pianobar.git
+echo "Cloning FFMpeg"
 git clone https://github.com/FFmpeg/FFmpeg.git
+echo "Cloning Pidora"
 git clone https://github.com/bbever/pidora.git
 
+echo "Compiling and Installing FFMPeg"
 cd FFmpeg/
 ./configure --enable-shared --disable-everything --enable-demuxer=mov --enable-decoder=aac --enable-protocol=http --enable-filter=volume --enable-filter=aformat --enable-filter=aresample --disable-programs --disable-doc
 make
@@ -11,10 +16,12 @@ sudo make install
 LD_LIBRARY_PATH=/usr/local/lib
 sudo ldconfig
 
+echo "Compiling and Installing Pianobar"
 cd ../pianobar
 make
 sudo make install
 
+echo "Starting configuration tasks"
 cd ..
 mkdir .config
 mkdir .config/pianobar
@@ -23,8 +30,13 @@ touch .config/pianobar/state
 
 sudo mkdir /.config
 sudo mkdir /.config/pianobar
-sudo ln -s /home/pi/.config/pianobar/state state
-sudo ln -s /home/pi/.config/pianobar/config config
+sudo ln -s /home/pi/.config/pianobar/state /.config/pianobar/state
+sudo ln -s /home/pi/.config/pianobar/config /.config/pianobar/config
+
+sudo mkdir /root/.config
+sudo mkdir /root/.config/pianobar
+sudo ln -s /home/pi/.config/pianobar/state /root/.config/pianobar/state
+sudo ln -s /home/pi/.config/pianobar/config /root/.config/pianobar/config
 
 
 cd $HOME
@@ -35,7 +47,7 @@ sleep 5
 clear
 echo "Starting to set up pianobar"
 read -p "What is your Pandora email address? " username
-read -p "What is your Pandora pasword? " password
+read -p "What is your Pandora password? " password
 echo "user = $username
 password = $password" > ~/.config/pianobar/config
 fingerprint=`openssl s_client -connect tuner.pandora.com:443 < /dev/null 2> /dev/null | openssl x509 -noout -fingerprint | tr -d ':' | cut -d'=' -f2` && echo tls_fingerprint = $fingerprint >> ~/.config/pianobar/config
